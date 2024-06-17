07d0c63b6c2377ef99def8f9554a8ec9085a8e5e
estiww <esti69771@gmail.com>
2024-06-17 17:33:10 +0300

    aa

--- server/routes/signupRoute.js
+++ server/routes/signupRoute.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const usersController = require('../controllers/usersController');
-
+const userscontroller = require('../controllers/usersController')
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 
-router.post("/", usersController.signup);
+router.post("/",userscontroller.authenticate);
 
 module.exports = router;
